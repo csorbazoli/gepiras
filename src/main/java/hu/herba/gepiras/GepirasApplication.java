12a461cbcdc6e2fa8dd77e42f559a664d6baebca
csorbazoli <csorbazoli@freemail.hu>
2017-08-01 08:17:35 +0200

    gepiras app generated by SpringInitializr

---
+++ src/main/java/hu/herba/gepiras/GepirasApplication.java
@@ -0,0 +1,12 @@
+package hu.herba.gepiras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GepirasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GepirasApplication.class, args);
+	}
+}
